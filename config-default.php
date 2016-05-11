33680d2d0d50aedd0578fc613e93b1a59d6fe880
Jinzy <zhengyu.jin@gmail.com>
2016-05-11 12:18:14 +0800

    Update jquery-ui version
    Fix the problem of:
    Exception_PathFinder: File not found
    Additional information:
    
        file: jquery-ui-1.11.master.js

--- config-default.php
+++ config-default.php
@@ -2,7 +2,7 @@
 $config['url_prefix']='?page=';
 $config['url_postfix']='';
 
-$config['js']['versions']['jqueryui']='1.11.master';
+$config['js']['versions']['jqueryui']='1.11.beta2.min';
 
 $config['dsn']='mysql://root:root@localhost/dam';
 
